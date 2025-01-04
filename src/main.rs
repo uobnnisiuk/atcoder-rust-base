59ee7b86e5839687aa8ea831bbc2759656f584b8
uobnnisiuk <uobnnisiuk@gmail.com>
2025-01-04 15:23:27 +0900

    1.70.0

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-// -*- coding:utf-8-unix -*-
-
 use proconio::input;
-
-// ABC086C - Traveling
-// https://atcoder.jp/contests/abs/tasks/arc089_a
 
 fn main() {
     input! {
--- tests/sample_inputs.rs
+++ tests/sample_inputs.rs
@@ -7,10 +7,12 @@ fn sample1() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
-        .output_with_stdin(r#"2
+        .output_with_stdin(
+            r#"2
 3 1 2
 6 1 1
-"#)
+"#,
+        )
         .tee_output()
         .expect_success();
     assert_eq!(output.stdout_str(), "Yes\n");
@@ -22,9 +24,11 @@ fn sample2() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
-        .output_with_stdin(r#"1
+        .output_with_stdin(
+            r#"1
 2 100 100
-"#)
+"#,
+        )
         .tee_output()
         .expect_success();
     assert_eq!(output.stdout_str(), "No\n");
@@ -36,13 +40,14 @@ fn sample3() {
     let testdir = TestDir::new(BIN, "");
     let output = testdir
         .cmd()
-        .output_with_stdin(r#"2
+        .output_with_stdin(
+            r#"2
 5 1 1
 100 1 1
-"#)
+"#,
+        )
         .tee_output()
         .expect_success();
     assert_eq!(output.stdout_str(), "No\n");
     assert!(output.stderr_str().is_empty());
 }
-
